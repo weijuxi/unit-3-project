8e950072bf2e3db116cb6478093de3d81ee5a16e
christiangit <colvera1218@gmail.com>
2024-10-09 15:21:59 -0500

    Added back end starter code

---
+++ server.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const testJWTRouter = require('./controllers/test-jwt');
+const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
+const reviewRouter = require('./controllers/reviews')
+
+// IMPORT MIDDLEWARE
+const verifyToken = require('./middleware/verify-token');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+app.use(cors());
+app.use(express.json());
+
+// Routes go here
+app.use('/test-jwt', testJWTRouter);
+app.use('/users', usersRouter);
+app.use('/profiles', profilesRouter);
+
+//this will make req.user available in every hoot controller function
+//this decodes the jwt
+app.use(verifyToken)
+app.use('/reviews', reviewRouter)
+
+app.listen(3000, () => {
+    console.log('The express app is ready!');
+});
