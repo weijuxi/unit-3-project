94ba67684b873ce85d4f50a2b0145867e3c95291
weijuxi <2568706310@qq.com>
2024-10-10 10:52:24 -0700

    add review create delete and update

--- controllers/movies.js
+++ controllers/movies.js
@@ -37,6 +37,52 @@ router.get('/:id', async function(req,res) {
     }
 })
 
+router.post('/:id/reviews', async function(req, res) {
+    console.log(req.body, '<- contents of the form')
+    console.log(req.user, '<- from the jwt')
+    try{
+        const movieDoc = await MovieModel.findById(req.params.id)
+
+        movieDoc.reviews.push(req.body)
+
+        await movieDoc.save()
+
+        res.status(201).json(movieDoc)
+    }catch(err){
+        res.status(500).json({ error: err.message });
+    }
+})
+
+
+router.put('/:id/reviews/:reviewId', async function(req, res) {
+    try{
+        const movieDoc = await MovieModel.findById(req.params.id)
+        const reviewDoc = movieDoc.reviews.id(req.params.reviewId)
+        reviewDoc.set(req.body)
+        await movieDoc.save()
+
+        res.status(200).json(movieDoc)
+    }catch(err){
+        res.status(500).json({ error: err.message });
+    }
+})
+
+
+router.delete('/:id/reviews/:reviewId', async function(req, res) {
+    console.log(req.body, '<---------------------this is from delete review')
+    console.log(req.user, '<---------------------this is from delete review')
+    try{
+        const movieDoc = await MovieModel.findById(req.params.id)
+        movieDoc.reviews.remove(req.params.reviewId)
+        await movieDoc.save()
+
+        res.status(200).json({ message: 'review deleted' })
+    }catch(err){
+        res.status(500).json({ error: err.message });
+    }
+})
+
+
 
 
 module.exports = router
--- models/movie.js
+++ models/movie.js
@@ -31,7 +31,7 @@ const movieSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    review: [reviewSchema]
+    reviews: [reviewSchema]
 })
 
 
