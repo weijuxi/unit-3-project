c9bf0f1eb6518d9ba5805f6ad23d287f35647299
loganrook <lbrook2003@gmail.com>
2024-10-15 12:06:16 -0400

    refactored movie model for images

--- models/movie.js
+++ models/movie.js
@@ -31,6 +31,10 @@ const movieSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    image {
+        type: String,
+        required: true,
+    },
     reviews: [reviewSchema]
 })
 
